6538cb076a28c570864478877bc012b5107bd437
FlareMarian <marian@flaregames.com>
2013-09-15 14:55:45 +0200

    More verbose configure recipes

--- battles/recipes/configure_usermgmt.rb
+++ battles/recipes/configure_usermgmt.rb
@@ -1,1 +1,9 @@
 Chef::Log.info("Should configure usermgmt")
+
+node[:opsworks][:layers][:mongo][:instances].each do |instance_name, instance|
+	Chef::Log.info("Found MongoDB instance #{instance_name} at #{instance[:private_ip]}")
+end
+
+node[:opsworks][:layers][:rabbitmq][:instances].each do |instance_name, instance|
+	Chef::Log.info("Found RabbitMQ instance #{instance_name} at #{instance[:private_ip]}")
+end
