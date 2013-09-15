68c512307726a384b617d7700924385c50b1c780
FlareMarian <marian@flaregames.com>
2013-09-15 16:10:39 +0200

    Added foo

--- battles/recipes/deploy_matchmaker.rb
+++ battles/recipes/deploy_matchmaker.rb
@@ -1,6 +1,13 @@
 if node[:deploy].attribute?(:matchmaker)
 
 	Chef::Log.info("Should deploy matchmaker")
-	Chef::Log.info("#{node[:deploy]}")
-
+	
+	def print_pair(parent, myHash)
+		myHash.each {|key, value|
+			value.is_a?(Hash) ? print_pair(key, value) :
+				Chef::Log.info("parent=#{parent.nil? ? 'none':parent}, (#{key}, #{value})")
+		}
+	end
+	
+	print_pair(nil,node)
 end
