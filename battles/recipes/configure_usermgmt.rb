1c63f36c5a187c5f5a0ed22b91d9b8cd563353da
FlareMarian <marian@flaregames.com>
2013-09-15 15:25:57 +0200

    Added memcached info

--- battles/recipes/configure_usermgmt.rb
+++ battles/recipes/configure_usermgmt.rb
@@ -7,3 +7,7 @@ end
 node[:opsworks][:layers][:rabbitmq][:instances].each do |instance_name, instance|
 	Chef::Log.info("Found RabbitMQ instance #{instance_name} at #{instance[:private_ip]}")
 end
+
+node[:opsworks][:layers][:memcached][:instances].each do |instance_name, instance|
+	Chef::Log.info("Found Memcached instance #{instance_name} at #{instance[:private_ip]}")
+end
