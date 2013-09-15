89e13ad5e2cfb059fb3c9a676cce9efd92e55ff1
FlareMarian <marian@flaregames.com>
2013-09-15 18:18:03 +0200

    Changed back

--- battles/attributes/default.rb
+++ battles/attributes/default.rb
@@ -1,17 +1,2 @@
 
-set[:mongodbhost] = nil
-set[:memcachedhost] = nil
-set[:rabbitmqhost] = nil
-
-node[:opsworks][:layers][:mongo][:instances].each do |instance_name, instance|
-	set[:mongodbhost] = instance[:private_ip]
-end
-
-node[:opsworks][:layers][:rabbitmq][:instances].each do |instance_name, instance|
-	set[:rabbitmqhost] = instance[:private_ip]
-end
-
-node[:opsworks][:layers][:memcached][:instances].each do |instance_name, instance|
-	set[:memcachedhost] = instance[:private_ip]
-end
 
