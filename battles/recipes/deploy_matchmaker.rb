aee40f68eb9f776e1088834687de12b05855198a
FlareMarian <marian@flaregames.com>
2013-09-15 16:36:29 +0200

    mm test

--- battles/recipes/deploy_matchmaker.rb
+++ battles/recipes/deploy_matchmaker.rb
@@ -1,13 +1,33 @@
-if node[:deploy].attribute?(:matchmaker)
+include_recipe 'deploy'
 
-	Chef::Log.info("Should deploy matchmaker")
-	
-	def print_pair(parent, myHash)
-		myHash.each {|key, value|
-			value.is_a?(Hash) ? print_pair(key, value) :
-				Chef::Log.info("parent=#{parent.nil? ? 'none':parent}, (#{key}, #{value})")
-		}
-	end
-	
-	print_pair(nil,node)
+node[:deploy].each do |application, deploy|
+
+  Chef::Log.info("Incoming deploy for #{application}")
+
+  if application != 'matchmaker'
+    Chef::Log.debug("Skipping deploy for application #{application} as it is not a matchmaker")
+    next
+  end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+
+  opsworks_nodejs do
+    deploy_data deploy
+    app application
+  end
+
+  ruby_block "restart node.js application #{application}" do
+    block do
+      Chef::Log.info("restart node.js via: #{node[:deploy][application][:nodejs][:restart_command]}")
+    end
+  end
 end
